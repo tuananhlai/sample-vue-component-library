444b57c966aeeee2612a0aa22bb9a3cc065b14ec
Tuan Anh Lai <tuananh.lai@xaiondata.co.jp>
2025-03-12 17:58:11 +0900

    chore/vite: include src/styles/tokens.css within dist/index.css

--- packages/core/src/index.ts
+++ packages/core/src/index.ts
@@ -1,1 +1,5 @@
+// import the CSS file here so that it will
+// be bundled with the component's CSS.
+import "./styles/tokens.css";
+
 export { Button } from "./components/button";
--- packages/core/vite.config.js
+++ packages/core/vite.config.js
@@ -16,21 +16,18 @@ export default defineConfig({
     }),
   ],
   build: {
+    // disable minification and sourcemap so that the library user can
+    // read the original source code more easily.
     minify: false,
     sourcemap: true,
     lib: {
       entry: resolve(__dirname, "src/index.ts"),
       name: "SampleDesignSystem",
-      // the proper extensions will be added
       fileName: "index",
     },
     rollupOptions: {
-      // make sure to externalize deps that shouldn't be bundled
-      // into your library
       external: ["vue"],
       output: {
-        // Provide global variables to use in the UMD build
-        // for externalized deps
         globals: {
           vue: "Vue",
         },
