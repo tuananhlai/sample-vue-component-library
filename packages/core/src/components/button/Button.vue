c81936d3cc35a0fcac8cfd8ffd92d0a0cb031ad1
Tuan Anh Lai <tuananh.lai@xaiondata.co.jp>
2025-03-12 16:21:17 +0900

    initialize project

---
+++ packages/core/src/components/button/Button.vue
@@ -0,0 +1,47 @@
+<script lang="ts">
+export interface ButtonProps {
+  /** @default 'primary' */
+  variant?: "primary" | "secondary" | "tertiary";
+}
+</script>
+
+<script setup lang="ts">
+withDefaults(defineProps<ButtonProps>(), {
+  variant: "primary",
+});
+</script>
+
+<template>
+  <button :class="$style.root">
+    <slot />
+  </button>
+</template>
+
+<style lang="scss" module>
+.root {
+  border-radius: var(--bw-radius-md);
+  font-size: var(--bw-font-base);
+  font-weight: var(--bw-weight-medium);
+  line-height: var(--bw-line-normal);
+  padding: var(--bw-space-2) var(--bw-space-4);
+  cursor: pointer;
+  border: none;
+  box-shadow: var(--bw-shadow-sm);
+
+  &:where([data-variant="primary"]) {
+    background: var(--bw-color-blue-600);
+    color: var(--bw-color-white);
+  }
+
+  &:where([data-variant="secondary"]) {
+    background: var(--bw-color-zinc-200);
+    color: var(--bw-color-zinc-800);
+  }
+
+  &:where([data-variant="tertiary"]) {
+    background: transparent;
+    color: var(--bw-color-blue-600);
+    box-shadow: none;
+  }
+}
+</style>
---
+++ packages/core/vite.config.js
@@ -0,0 +1,32 @@
+import vue from "@vitejs/plugin-vue";
+import { dirname, resolve } from "node:path";
+import { fileURLToPath } from "node:url";
+import { defineConfig } from "vite";
+import dts from "vite-plugin-dts";
+
+const __dirname = dirname(fileURLToPath(import.meta.url));
+
+export default defineConfig({
+  plugins: [vue(), dts()],
+  build: {
+    minify: false,
+    lib: {
+      entry: resolve(__dirname, "src/index.ts"),
+      name: "SampleDesignSystem",
+      // the proper extensions will be added
+      fileName: "index",
+    },
+    rollupOptions: {
+      // make sure to externalize deps that shouldn't be bundled
+      // into your library
+      external: ["vue"],
+      output: {
+        // Provide global variables to use in the UMD build
+        // for externalized deps
+        globals: {
+          vue: "Vue",
+        },
+      },
+    },
+  },
+});
