5b1837e75865323bd56e52106ae9bbf83749a3d4
Lại Tuấn Anh <42168740+tuananhlai@users.noreply.github.com>
2025-03-13 13:40:37 +0900

    chore/changeset/initialize (#3)
    * fix/ci: failed due to fetching of private packages
    
    * chore/changeset: initialize
    
    * minor changes
    
    * minor changes
    
    * minor changes
    
    * minor changes

---
+++ examples/dogfood/CHANGELOG.md
@@ -0,0 +1,8 @@
+# dogfood
+
+## 0.0.1
+
+### Patch Changes
+
+- Updated dependencies [0da11ff]
+  - @tuananhlai/sample-design-system@0.0.3
--- packages/core/src/components/button/Button.vue
+++ packages/core/src/components/button/Button.vue
@@ -27,21 +27,35 @@ withDefaults(defineProps<ButtonProps>(), {
   cursor: pointer;
   border: none;
   box-shadow: var(--bw-shadow-sm);
+  --btn-hover-background-color: initial;
 
   &:where([data-variant="primary"]) {
     background: var(--bw-button-primary-background-color);
     color: var(--bw-button-primary-color);
+    --btn-hover-background-color: var(
+      --bw-button-primary-hover-background-color
+    );
   }
 
   &:where([data-variant="secondary"]) {
     background: var(--bw-button-secondary-background-color);
     color: var(--bw-button-secondary-color);
+    --btn-hover-background-color: var(
+      --bw-button-secondary-hover-background-color
+    );
   }
 
   &:where([data-variant="tertiary"]) {
     background: var(--bw-button-tertiary-background-color);
     color: var(--bw-button-tertiary-color);
     box-shadow: none;
+    --btn-hover-background-color: var(
+      --bw-button-tertiary-hover-background-color
+    );
+  }
+
+  &:where(:hover) {
+    background-color: var(--btn-hover-background-color);
   }
 }
 </style>
