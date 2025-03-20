3f060293e6a61c34eb798c7bff7b06b5ae296964
Tuan Anh Lai <tuananh.lai@xaiondata.co.jp>
2025-03-20 11:56:27 +0900

    minor changes

--- packages/core/tests/TextField.test.ts
+++ packages/core/tests/TextField.test.ts
@@ -1,5 +1,6 @@
 import { cleanup, render, screen } from "@testing-library/vue";
 import { afterEach, describe, expect, it } from "vitest";
+import { nextTick } from 'vue';
 import { TextField } from "../src";
 
 describe("TextField", () => {
@@ -13,13 +14,15 @@ describe("TextField", () => {
     expect(screen.getByRole("textbox")).toBeInTheDocument();
   });
 
-  it("should be associated with label", () => {
+  it("should be associated with label", async () => {
     render(TextField, {
       props: {
         label: "example",
       },
     });
 
+    await nextTick();
+    
     expect(
       screen.getByRole("textbox", { name: /example/i })
     ).toBeInTheDocument();
