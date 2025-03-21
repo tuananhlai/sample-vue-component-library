9946fb9cfe802133397feccaf0030cb2b3a3f39b
Lại Tuấn Anh <42168740+tuananhlai@users.noreply.github.com>
2025-03-21 09:48:10 +0900

    feat/text field: add basic unit tests (#13)
    * feat/text-field: add test
    
    * minor changes
    
    * minor changes
    
    * minor changes
    
    * minor changes
    
    * minor changes

--- packages/core/tests/TextField.test.ts
+++ packages/core/tests/TextField.test.ts
@@ -1,6 +1,88 @@
-import { it, expect } from 'vitest';
-import { TextField } from '../src';
+import userEvent from "@testing-library/user-event";
+import {
+  cleanup,
+  render,
+  RenderOptions,
+  RenderResult,
+  screen,
+} from "@testing-library/vue";
+import { afterEach, describe, expect, it, vi } from "vitest";
+import { nextTick } from "vue";
+import { TextField } from "../src";
 
-it('should render', () => {
-  expect(true).toBe(true);
+describe("TextField", () => {
+  afterEach(() => {
+    cleanup();
+  });
+
+  it("should render", async () => {
+    await renderTextField({});
+
+    expect(screen.getByRole("textbox")).toBeInTheDocument();
+  });
+
+  it("should render a label and associate it with the input when `label` prop is passed", async () => {
+    await renderTextField({
+      props: {
+        label: "example",
+      },
+    });
+
+    expect(
+      screen.getByRole("textbox", { name: /example/i })
+    ).toBeInTheDocument();
+  });
+
+  it("should render a description when `description` prop is passed", async () => {
+    await renderTextField({
+      props: {
+        description: "example",
+      },
+    });
+
+    const input = screen.getByRole("textbox");
+    expect(input).toHaveAccessibleDescription("example");
+  });
+
+  it("should render an error message when `errorMessage` prop is passed", async () => {
+    await renderTextField({
+      props: {
+        errorMessage: "error",
+      },
+    });
+
+    const input = screen.getByRole("textbox");
+    expect(input).toHaveAccessibleDescription("error");
+  });
+
+  it("should work with `v-model`", async () => {
+    const onUpdate = vi.fn();
+
+    await renderTextField({
+      props: {
+        modelValue: "example",
+        "onUpdate:modelValue": onUpdate,
+      },
+    });
+
+    const input = screen.getByRole("textbox");
+    expect(input).toHaveValue("example");
+
+    await userEvent.type(input, "test");
+
+    expect(onUpdate).toHaveBeenCalledWith("exampletest");
+  });
 });
+
+const renderTextField = async (
+  opts: RenderOptions<typeof TextField>
+): Promise<RenderResult> => {
+  const renderResult = render(TextField, opts);
+
+  // Wait for `onMounted` to finish before continuing with
+  // the assertions.
+  // TODO: Figure out a way to avoid having to called `nextTick`.
+  await nextTick();
+
+  return renderResult;
+};
